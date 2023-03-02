2a659dabd8c4e2ee31cdd5e2e16386caac1dd4e0
Gregory <danidveloper@gmail.com>
2023-03-02 04:58:22 -0500

    Agregando 01-Bases de Angular

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+
+
+
+@NgModule({
+    declarations : [
+        HeroeComponent,
+        ListadoComponent
+    ],
+    exports : [
+        ListadoComponent
+    ],
+    imports : [
+        CommonModule
+        //Para poder usar las directivas *neg
+    ]
+})
+export class HeroesModule {
+
+}
